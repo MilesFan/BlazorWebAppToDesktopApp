25cbef4910f222a4486baf281ec9dfd45da60b8f
Miles Fan <milesfan@qq.com>
2026-07-14 04:40:36 +0800

    pass arguments

--- DesktopApp/main.c
+++ DesktopApp/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <windows.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef void(__cdecl* run_fn)(int argc, char* argv[]);
+
+void FreeArgv(int argc, char** argv) {
+	if (argv == NULL) return;
+	for (int i = 0; i < argc; i++) {
+		free(argv[i]);
+	}
+	free(argv);
+}
 
 void* LoadSymbol(HMODULE module, const char* name)
 {
@@ -27,7 +37,6 @@ int main(int argc, char* argv[])
 	run(argc, argv);
 	return 0;
 }
-
 int WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -35,5 +44,41 @@ int WinMain(
 	_In_ int nShowCmd
 )
 {
-	main(0, NULL);
+	int argc = 0;
+	char** argv = NULL;
+	LPWSTR* szArglistW = NULL;
+	char exePath[MAX_PATH];
+
+	GetModuleFileNameA(NULL, exePath, MAX_PATH);
+
+	szArglistW = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+	if (szArglistW == NULL) {
+		MessageBoxA(NULL, "Failed to parse command line arguments", "Error", MB_ICONERROR);
+		return -1;
+	}
+
+	argv = (char**)malloc(argc * sizeof(char*));
+	if (argv == NULL) {
+		LocalFree(szArglistW);
+		return -1;
+	}
+
+	argv[0] = _strdup(exePath);
+
+	for (int i = 1; i < argc; i++) {
+		int len = WideCharToMultiByte(CP_ACP, 0, szArglistW[i], -1, NULL, 0, NULL, NULL);
+		argv[i] = (char*)malloc(len);
+		if (argv[i]) {
+			WideCharToMultiByte(CP_ACP, 0, szArglistW[i], -1, argv[i], len, NULL, NULL);
+		}
+	}
+
+	LocalFree(szArglistW);
+
+	int result = main(argc, argv);
+
+	FreeArgv(argc, argv);
+
+	return result;
 }
